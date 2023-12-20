b2c587dec71fe624afbd198f7ab4a150705bb7cb
candra-tr <ctriatna@gmail.com>
2023-12-20 23:13:58 +0700

    ass register

--- Ecommerce-Inventory/resources/views/auth/register.blade.php
+++ Ecommerce-Inventory/resources/views/auth/register.blade.php
@@ -20,6 +20,16 @@
             </div>
 
             <div class="mt-4">
+                <x-label for="phone" value="{{ __('Email') }}" />
+                <x-input id="phone" class="block mt-1 w-full" type="number" name="number" :value="old('phone')" required autocomplete="username" />
+            </div>
+
+            <div class="mt-4">
+                <x-label for="email" value="{{ __('Email') }}" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            </div>
+
+            <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>

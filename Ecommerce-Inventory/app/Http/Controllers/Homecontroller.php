4247fa972ba32abb0e6484feea1e31f8922c484e
candra-tr <ctriatna@gmail.com>
2023-12-21 06:57:42 +0700

    add user admin

---
+++ Ecommerce-Inventory/app/Http/Controllers/Homecontroller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function redirect()
+    {
+        // Check if the user is authenticated
+        if (Auth::check()) {
+            // Retrieve the user type
+            $usertype = Auth::user()->usertype;
+
+            // Determine the view based on the user type
+            if ($usertype == '1') {
+                return view('admin.home');
+            } else {
+                return view('dashboard');
+            }
+        }
+
+        // If the user is not authenticated,  might want to redirect them to the login page
+        return redirect()->route('login');
+    }
+}

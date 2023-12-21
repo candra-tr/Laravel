fc40d5d3d67f9b9a5cabfaedeeaa9e9e84a3d131
candra-tr <ctriatna@gmail.com>
2023-12-21 07:37:51 +0700

    edit dashboard to redirect

--- Ecommerce-Inventory/resources/views/admin/home.blade.php
+++ Ecommerce-Inventory/resources/views/admin/home.blade.php
@@ -1,11 +1,14 @@
+<x-app-layout>
+
+</x-app-layout>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Admin Dashboard</h1>
+<h1>Admin</h1>
 </body>
 </html>
